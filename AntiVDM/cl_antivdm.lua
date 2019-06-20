f1d55eb22af57610f28762bcca159e2779268c72
Roberto Barra <robertobarra98@gmail.com>
2019-06-20 23:49:21 +0100

    AntiVDM script, (getting hit by vehicles does 0 damage)

---
+++ AntiVDM/cl_antivdm.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+    while true do
+        N_0x4757f00bc6323cfe(-1553120962, 0.0) --undocumented damage modifier. 1st argument is hash, 2nd is modified (0.0-1.0)
+        Wait(0)
+    end
+end)
