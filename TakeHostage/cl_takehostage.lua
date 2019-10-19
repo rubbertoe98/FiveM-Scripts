89e02bde4071f63f442ce98574d1f216dff023bb
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-19 16:52:38 +0100

    optimise closest player func

--- TakeHostage/cl_takehostage.lua
+++ TakeHostage/cl_takehostage.lua
@@ -146,21 +146,22 @@ function GetClosestPlayer(radius)
     local players = GetActivePlayers()
     local closestDistance = -1
     local closestPlayer = -1
-    local ply = GetPlayerPed(-1)
-    local plyCoords = GetEntityCoords(ply, 0)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed, false)
 
-    for index,value in ipairs(players) do
-        local target = GetPlayerPed(value)
-        if(target ~= ply) then
-            local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
-            local distance = GetDistanceBetweenCoords(targetCoords['x'], targetCoords['y'], targetCoords['z'], plyCoords['x'], plyCoords['y'], plyCoords['z'], true)
-            if(closestDistance == -1 or closestDistance > distance) then
-                closestPlayer = value
+    for i = 1, #players do
+        local targetPed = GetPlayerPed(players[i])
+        if playerPed ~= targetPed then
+            local targetCoords = GetEntityCoords(targetPed, false)
+            local distance = #(playerCoords - targetCoords)
+
+            if closestDistance == -1 or closestDistance > distance then
+                closestPlayer = players[i]
                 closestDistance = distance
             end
         end
     end
-	print("closest player is dist: " .. tostring(closestDistance))
+
 	if closestDistance <= radius then
 		return closestPlayer
 	else
