3e63be606f672446f58a56dc10ed01d8fee95f1b
Robbster <robertobarra98@gmail.com>
2020-01-06 21:20:37 +0000

    CarryPeople: force animation if cancelled by another script

--- CarryPeople/cl_carry.lua
+++ CarryPeople/cl_carry.lua
@@ -1,4 +1,7 @@
 local carryingBackInProgress = false
+local carryAnimNamePlaying = ""
+local carryAnimDictPlaying = ""
+local carryControlFlagPlaying = 0
 
 RegisterCommand("carry",function(source, args)
 	if not carryingBackInProgress then
@@ -20,7 +23,6 @@ RegisterCommand("carry",function(source, args)
 		target = GetPlayerServerId(closestPlayer)
 		if closestPlayer ~= 0 then
 			TriggerServerEvent('CarryPeople:sync', closestPlayer, lib,lib2, anim1, anim2, distans, distans2, height,target,length,spin,controlFlagMe,controlFlagTarget,animFlagTarget)
-		else
 		end
 	else
 		carryingBackInProgress = false
@@ -48,6 +50,9 @@ AddEventHandler('CarryPeople:syncTarget', function(target, animationLib, animati
 	AttachEntityToEntity(GetPlayerPed(-1), targetPed, 0, distans2, distans, height, 0.5, 0.5, spin, false, false, false, false, 2, false)
 	if controlFlag == nil then controlFlag = 0 end
 	TaskPlayAnim(playerPed, animationLib, animation2, 8.0, -8.0, length, controlFlag, 0, false, false, false)
+	carryAnimNamePlaying = animation2
+	carryAnimDictPlaying = animationLib
+	carryControlFlagPlaying = controlFlag
 end)
 
 RegisterNetEvent('CarryPeople:syncMe')
@@ -61,8 +66,9 @@ AddEventHandler('CarryPeople:syncMe', function(animationLib, animation,length,co
 	Wait(500)
 	if controlFlag == nil then controlFlag = 0 end
 	TaskPlayAnim(playerPed, animationLib, animation, 8.0, -8.0, length, controlFlag, 0, false, false, false)
-
-	Citizen.Wait(length)
+	carryAnimNamePlaying = animation
+	carryAnimDictPlaying = animationLib
+	carryControlFlagPlaying = controlFlag
 end)
 
 RegisterNetEvent('CarryPeople:cl_stop')
@@ -70,6 +76,18 @@ AddEventHandler('CarryPeople:cl_stop', function()
 	carryingBackInProgress = false
 	ClearPedSecondaryTask(GetPlayerPed(-1))
 	DetachEntity(GetPlayerPed(-1), true, false)
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		if carryingBackInProgress then 
+			while not IsEntityPlayingAnim(GetPlayerPed(-1), carryAnimDictPlaying, carryAnimNamePlaying, 3) do
+				TaskPlayAnim(GetPlayerPed(-1), carryAnimDictPlaying, carryAnimNamePlaying, 8.0, -8.0, 100000, carryControlFlagPlaying, 0, false, false, false)
+				Citizen.Wait(0)
+			end
+		end
+		Wait(0)
+	end
 end)
 
 function GetPlayers()
