c2b63aa28af5f35ff82549822b2757fe9bce962a
Rob <11280253+rubbertoe98@users.noreply.github.com>
2020-05-24 20:06:17 +0100

    add: optimization & infinity support & voice_markers

--- IDsAboveHead/cl_idsabovehead.lua
+++ IDsAboveHead/cl_idsabovehead.lua
@@ -1,55 +1,21 @@
 local disPlayerNames = 5
+local playerDistances = {}
 
-playerDistances = {}
-
-
-Citizen.CreateThread(function()
-    Wait(50)
-    while true do
-        for id = 0, 255 do 
-			if NetworkIsPlayerActive(id) then
-				if GetPlayerPed(id) ~= GetPlayerPed(-1) then
-					if (playerDistances[id] < disPlayerNames) then
-						x2, y2, z2 = table.unpack(GetEntityCoords(GetPlayerPed(id), true))
-						if NetworkIsPlayerTalking(id) then
-							DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 247,124,24)
-						else
-							DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 255,255,255)
-						end
-					end  
-				end
-			end
-        end
-        Citizen.Wait(0)
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        for id = 0, 255 do
-            if GetPlayerPed(id) ~= GetPlayerPed(-1) then
-                x1, y1, z1 = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-                x2, y2, z2 = table.unpack(GetEntityCoords(GetPlayerPed(id), true))
-                distance = math.floor(GetDistanceBetweenCoords(x1,  y1,  z1,  x2,  y2,  z2,  true))
-				playerDistances[id] = distance
-            end
-        end
-        Citizen.Wait(1000)
-    end
-end)
-
-
-function DrawText3D(x,y,z, text, r,g,b) 
+local function DrawText3D(x,y,z, text, r,g,b) 
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
     local px,py,pz=table.unpack(GetGameplayCamCoords())
-    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+    local dist = #(vector3(px,py,pz)-vector3(x,y,z))
  
     local scale = (1/dist)*2
     local fov = (1/GetGameplayCamFov())*100
     local scale = scale*fov
    
     if onScreen then
-        SetTextScale(0.0*scale, 0.55*scale)
+        if not useCustomScale then
+            SetTextScale(0.0*scale, 0.55*scale)
+        else 
+            SetTextScale(0.0*scale, customScale)
+        end
         SetTextFont(0)
         SetTextProportional(1)
         SetTextColour(r, g, b, 255)
@@ -63,3 +29,44 @@ function DrawText3D(x,y,z, text, r,g,b)
         DrawText(_x,_y)
     end
 end
+
+Citizen.CreateThread(function()
+	Wait(500)
+    while true do
+        for _, id in ipairs(GetActivePlayers()) do
+				if GetPlayerPed(id) ~= GetPlayerPed(-1) then
+					if playerDistances[id] then
+						if (playerDistances[id] < disPlayerNames) then
+							x2, y2, z2 = table.unpack(GetEntityCoords(GetPlayerPed(id), true))
+							if NetworkIsPlayerTalking(id) then
+                                DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 247,124,24)
+								DrawMarker(27, x2, y2, z2-0.97, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 173, 216, 230, 100, 0, 0, 0, 0)
+							else
+								DrawText3D(x2, y2, z2+1, GetPlayerServerId(id), 255,255,255)
+							end
+						elseif (playerDistances[id] < 25) then
+							x2, y2, z2 = table.unpack(GetEntityCoords(GetPlayerPed(id), true))						
+							if NetworkIsPlayerTalking(id) then
+                                DrawMarker(27, x2, y2, z2-0.97, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 173, 216, 230, 100, 0, 0, 0, 0)
+							end
+						end
+					end
+				end
+        end
+        Citizen.Wait(0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        for _, id in ipairs(GetActivePlayers()) do
+            if GetPlayerPed(id) ~= GetPlayerPed(-1) then
+                x1, y1, z1 = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+                x2, y2, z2 = table.unpack(GetEntityCoords(GetPlayerPed(id), true))
+                distance = math.floor(#(vector3(x1,  y1,  z1)-vector3(x2,  y2,  z2)))
+				playerDistances[id] = distance
+            end
+        end
+        Citizen.Wait(1000)
+    end
+end)
