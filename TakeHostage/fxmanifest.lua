b3b48d1284ea7eb1db04fdc715543a22821c75de
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 18:58:24 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ TakeHostage/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'rubbertoe98'
+description 'TakeHostage'
+version '1.0.0'
+
+client_script "cl_takehostage.lua"
+server_script "sv_takehostage.lua"
