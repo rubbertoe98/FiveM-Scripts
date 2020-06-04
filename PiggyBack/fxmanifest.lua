9251b6b8176a582501edbed11260ab94f0035221
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 18:57:58 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ PiggyBack/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'rubbertoe98'
+description 'PiggyBack'
+version '1.0.0'
+
+client_script "cl_piggyback.lua"
+server_script "sv_piggyback.lua"
