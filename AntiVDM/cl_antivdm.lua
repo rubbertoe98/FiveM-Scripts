5625bdbe0f2be9c39c2a1aec42abd19f9424c463
Vyast <67253117+Vyast@users.noreply.github.com>
2021-02-22 00:50:15 -0700

    Delete AntiVDM directory

--- AntiVDM/cl_antivdm.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        SetWeaponDamageModifier(-1553120962, 0.0)
-        Wait(0)
-    end
-end)
--- AntiVDM/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
--- Resource Metadata
-fx_version 'bodacious'
-games { 'gta5' }
-
-author 'rubbertoe98'
-description 'AntiVDM'
-version '1.0.0'
-
-client_script "cl_antivdm.lua"
