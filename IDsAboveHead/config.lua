eddbf8f8eda9b5912c5083c2c98ea493f5b24f17
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-08-12 19:40:14 -0700

    Added config.lua
    Added easy-to-follow configuration.

---
+++ IDsAboveHead/config.lua
@@ -0,0 +1,10 @@
+Config = {
+    Prefix = "Server Name", -- Sets the prefix for the chat messages.
+    Distance = 5, -- Sets the distance where a player's ID can be viewed.
+    Blip = true, -- True enables the blip that appears below the player, false disables it.
+    AlwaysDisplayID = true, -- True makes it where the ID will always show above their head even if they are not speaking, false disables it.
+
+    -- Commands Available:
+    -- /toggle-id
+    ToggleCommand = "toggle-id"
+}
